be80fedd7cbaaea694753a576e325037a35c558e
Dave Andersen <d.andersen@gmail.com>
2012-02-06 23:17:02 -0800

    Add optional reminder to CalendarGenerator.
    Adds an optional argument for calendarFromMatches and
    eventFromMatch which sets the number of minutes before the
    reminder is displayed.

--- CalendarGenerator.py
+++ CalendarGenerator.py
@@ -1,7 +1,7 @@
 from datetime import timedelta
 import logging
 
-from icalendar import Calendar, Event
+from icalendar import Calendar, Event, Alarm
 
 import ScheduleProcessor
 
@@ -22,19 +22,31 @@ def eventFromMatch(match, team, reminder=None):
     evt.add('summary', 'Soccer - {0} vs. {1}'.format(color, opponent))
     evt.add('dtstart', match.time)
     evt.add('dtend', match.time + timedelta(hours = 1))
+
+    if reminder:
+        evt.add_component(createReminder(reminder))
+
     return evt
 
-def calendarFromMatches(matches, team):
+def calendarFromMatches(matches, team, reminder=None):
     cal = Calendar()
     cal.add('version', '2.0')
     cal.add('prodid', '-//com/appspot/SambaSchedule//NONSGML v1.0//EN')
     for match in matches:
-        evt = eventFromMatch(match, team)
+        evt = eventFromMatch(match, team, reminder)
         if evt:
             cal.add_component(evt)
     return cal
+
+def createReminder(minutes):
+    a = Alarm()
+    a.add('ACTION', 'DISPLAY')
+    a.add('DESCRIPTION', 'starting soon...')
+    a.add('TRIGGER', timedelta(minutes = -minutes))
+    return a
 
 if __name__ == '__main__':
     html = open('sample.html').read()
     matches = ScheduleProcessor.processPage(html)
     cal = calendarFromMatches(matches, 'The Captain')
+    a = createReminder(35)
