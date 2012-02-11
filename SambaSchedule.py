06f6441ddc8330cdb006f67a766e633fb128269c
Dave Andersen <d.andersen@gmail.com>
2012-02-10 23:13:05 -0800

    convert spaces in links to underscores

--- SambaSchedule.py
+++ SambaSchedule.py
@@ -14,13 +14,15 @@ jinja_environment = jinja2.Environment(
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
-        template_values = {'teams': teams}
+        template_values = {'teams': teams,
+            'url_format': UrlFormatTeamName}
         template = jinja_environment.get_template('index.html')
         self.response.out.write(template.render(template_values))
 
 
 class TeamSchedule(webapp2.RequestHandler):
     def get(self, team_name):
+        team_name = UrlUnformatTeamName(team_name)
         template = jinja_environment.get_template('schedule.html')
         template_values = {
             'team_name': team_name,
@@ -28,13 +30,15 @@ class TeamSchedule(webapp2.RequestHandler):
                        'color':m.color(team_name),
                        'opponent':m.opponent(team_name)}
                       for m in matches
-                      if m.contains_team(team_name)]
+                      if m.contains_team(team_name)],
+            'url_format': UrlFormatTeamName
             }
         self.response.out.write(template.render(template_values))
 
 
 class TeamCalendar(webapp2.RequestHandler):
     def get(self, team_name):
+        team_name = UrlUnformatTeamName(team_name)
         try:
             reminder = int(self.request.get('reminder'))
         except:
@@ -42,6 +46,12 @@ class TeamCalendar(webapp2.RequestHandler):
         self.response.out.write(
             str(CalendarGenerator.calendarFromMatches(
                 matches, team_name, reminder)))
+
+def UrlFormatTeamName(team_name):
+    return team_name.replace(' ', '_')
+
+def UrlUnformatTeamName(team_name):
+    return team_name.replace('_', ' ')
 
 html = open('sample.html').read()
 
